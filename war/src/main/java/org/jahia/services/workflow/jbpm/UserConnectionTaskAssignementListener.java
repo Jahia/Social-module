e8540170d00c5373abe037f09c44cd6a945cdcb4
Thomas Draier <tdraier@jahia.com>
2012-04-18 16:26:55 +0200

    changed dependency, create task when asking a user connection

--- war/src/main/java/org/jahia/services/workflow/jbpm/UserConnectionTaskAssignementListener.java
+++ war/src/main/java/org/jahia/services/workflow/jbpm/UserConnectionTaskAssignementListener.java
@@ -42,29 +42,37 @@ package org.jahia.services.workflow.jbpm;
 
 import org.apache.commons.lang.StringUtils;
 import org.jahia.registries.ServicesRegistry;
+import org.jahia.services.usermanager.JahiaPrincipal;
 import org.jbpm.api.model.OpenExecution;
 import org.jbpm.api.task.Assignable;
-import org.jbpm.api.task.AssignmentHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Assignment handler for user connection task.
  * 
  * @author Serge Huber
  */
-public class UserConnectionTaskAssignementListener implements AssignmentHandler {
+public class UserConnectionTaskAssignementListener extends JBPMTaskAssignmentListener {
     
     private static final long serialVersionUID = 3356236148908996978L;
 
     /**
      * sets the actorId and candidates for the given task.
      */
-    public void assign(Assignable assignable, OpenExecution execution) throws Exception {
+    public void assign(final Assignable assignable, OpenExecution execution) throws Exception {
 
         String to = (String) execution.getVariable("to");
         if (StringUtils.isNotEmpty(to)) {
             assignable.addCandidateUser(to);
         }
+        List<JahiaPrincipal> p = new ArrayList<JahiaPrincipal>();
+        p.add(ServicesRegistry.getInstance().getJahiaUserManagerService().lookupUser(to));
+
         assignable.addCandidateGroup(ServicesRegistry.getInstance().getJahiaGroupManagerService()
                 .getAdministratorGroup(0).getGroupKey());
+
+        createTask(assignable, execution, p);
     }
 }
