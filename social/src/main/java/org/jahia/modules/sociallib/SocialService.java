c91ff0091e478d41f9fcede582bfd46d41dd1532
Thomas Draier <tdraier@jahia.com>
2014-11-10 13:07:31 +0100

    QA-6669 :  use separate bean for post processing activity recorder beans, to avoid osgi service initialization issues

--- social/src/main/java/org/jahia/modules/sociallib/SocialService.java
+++ social/src/main/java/org/jahia/modules/sociallib/SocialService.java
@@ -101,7 +101,7 @@ import java.util.*;
  *
  * @author Serge Huber
  */
-public class SocialService implements JahiaModulesBeanPostProcessor {
+public class SocialService {
 
     private static Logger logger = LoggerFactory.getLogger(SocialService.class);
     public static final String JNT_BASE_SOCIAL_ACTIVITY = "jnt:baseSocialActivity";
@@ -128,7 +128,11 @@ public class SocialService implements JahiaModulesBeanPostProcessor {
     private WorkflowService workflowService;
     private JCRContentUtils jcrContentUtils;
 
-    private Map<String, ActivityRecorder> activityRecorderMap = new LinkedHashMap<String, ActivityRecorder>();
+    private ActivityRecorderRegistry activityRecorderRegistry;
+
+    public void setActivityRecorderRegistry(ActivityRecorderRegistry activityRecorderRegistry) {
+        this.activityRecorderRegistry = activityRecorderRegistry;
+    }
 
     public void addActivity(final String userKey, final String message, JCRSessionWrapper session) throws RepositoryException {
         addActivity(userKey, null, "text", session, message);
@@ -150,8 +154,8 @@ public class SocialService implements JahiaModulesBeanPostProcessor {
     public JCRNodeWrapper addActivity(final JCRUserNode userNode, final JCRNodeWrapper targetNode, String activityType, JCRSessionWrapper session, Object... args) throws RepositoryException {
         JCRNodeWrapper activitiesNode = getActivitiesNode(session, userNode);
 
-        if (activityRecorderMap.containsKey(activityType)) {
-            ActivityRecorder activityRecorder = activityRecorderMap.get(activityType);
+        if (activityRecorderRegistry.getActivityRecorderMap().containsKey(activityType)) {
+            ActivityRecorder activityRecorder = activityRecorderRegistry.getActivityRecorderMap().get(activityType);
             String nodeType = activityRecorder.getNodeTypeForActivity(activityType);
             String nodeName = jcrContentUtils.generateNodeName(activitiesNode, nodeType)+"_"+((int) Math.rint(Math.random()*100000));
             JCRNodeWrapper activityNode = activitiesNode.addNode(nodeName, nodeType);
@@ -517,7 +521,7 @@ public class SocialService implements JahiaModulesBeanPostProcessor {
     }
 
     public Map<String, ActivityRecorder> getActivityRecorderMap() {
-        return activityRecorderMap;
+        return activityRecorderRegistry.getActivityRecorderMap();
     }
 
     /**
@@ -552,63 +556,8 @@ public class SocialService implements JahiaModulesBeanPostProcessor {
         }
     }
 
-    /**
-     * Apply this BeanPostProcessor to the given new bean instance <i>after</i> any bean
-     * initialization callbacks (like InitializingBean's <code>afterPropertiesSet</code>
-     * or a custom init-method). The bean will already be populated with property values.
-     * The returned bean instance may be a wrapper around the original.
-     * <p>In case of a FactoryBean, this callback will be invoked for both the FactoryBean
-     * instance and the objects created by the FactoryBean (as of Spring 2.0). The
-     * post-processor can decide whether to apply to either the FactoryBean or created
-     * objects or both through corresponding <code>bean instanceof FactoryBean</code> checks.
-     * <p>This callback will also be invoked after a short-circuiting triggered by a
-     * {@link org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation} method,
-     * in contrast to all other BeanPostProcessor callbacks.
-     *
-     * @param bean     the new bean instance
-     * @param beanName the name of the bean
-     * @return the bean instance to use, either the original or a wrapped one; if
-     *         <code>null</code>, no subsequent BeanPostProcessors will be invoked
-     * @throws org.springframework.beans.BeansException
-     *          in case of errors
-     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet
-     * @see org.springframework.beans.factory.FactoryBean
-     */
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (bean instanceof ActivityRecorder) {
-            ActivityRecorder activityRecorder = (ActivityRecorder) bean;
-            for (String activityType : activityRecorder.getActivityTypes().keySet()) {
-                activityRecorderMap.put(activityType, activityRecorder);
-            }
-        }
-        return bean;
-    }
-
-    /**
-     * Apply this BeanPostProcessor to the given new bean instance <i>before</i> any bean
-     * initialization callbacks (like InitializingBean's <code>afterPropertiesSet</code>
-     * or a custom init-method). The bean will already be populated with property values.
-     * The returned bean instance may be a wrapper around the original.
-     *
-     * @param bean     the new bean instance
-     * @param beanName the name of the bean
-     * @return the bean instance to use, either the original or a wrapped one; if
-     *         <code>null</code>, no subsequent BeanPostProcessors will be invoked
-     * @throws org.springframework.beans.BeansException
-     *          in case of errors
-     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet
-     */
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return bean;
-    }
-
     private boolean execute(JCRCallback<Boolean> jcrCallback) throws RepositoryException {
         return JCRTemplate.getInstance().doExecuteWithSystemSession(jcrCallback);
     }
 
-
-    @Override
-    public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
-
-    }
 }
